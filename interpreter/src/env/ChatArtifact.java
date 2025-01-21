d28350768678992fdc0458bb05f667bd7785ff2d
Andrea Gatti <andrea.gatti@edu.unige.it>
2025-01-21 15:05:10 +0100

    Chat interface improvements and management

--- interpreter/src/env/ChatArtifact.java
+++ interpreter/src/env/ChatArtifact.java
@@ -1,5 +1,7 @@
 package env;
 
+import com.formdev.flatlaf.FlatLightLaf;
+
 import cartago.*;
 import cartago.tools.GUIArtifact;
 import jason.asSyntax.*;
@@ -9,12 +11,16 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
+import javax.swing.JTextPane;
 import javax.swing.JTextField;
 import javax.swing.SwingUtilities;
 
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class ChatArtifact extends GUIArtifact {
 
@@ -24,12 +30,11 @@ public class ChatArtifact extends GUIArtifact {
     public void init() {
         view = new ChatView( this );
         view.setVisible(true);
-        defineObsProperty("user_msg", "");
     }
 
     @OPERATION
     public void msg(String sender, String message) {
-        view.appendMessage("[" + sender + "] " + message);
+        view.appendMessage(sender, message);
     }
 
     @INTERNAL_OPERATION
@@ -39,11 +44,13 @@ public class ChatArtifact extends GUIArtifact {
 
     class ChatView extends JFrame {
         private ChatArtifact art;
-        private JTextArea chatArea;
+        // private JTextArea chatArea;
+        private JTextPane chatPane;
         private JTextField inputField;
         private JButton sendButton;
 
         public ChatView( ChatArtifact art ) {
+            FlatLightLaf.setup();
             this.art = art;
 
             setTitle("SpeakAgent");
@@ -51,11 +58,15 @@ public class ChatArtifact extends GUIArtifact {
             setSize(400, 500);
             setLayout(new BorderLayout());
 
-            chatArea = new JTextArea();
-            chatArea.setEditable(false);
-            chatArea.setLineWrap(true);
-            chatArea.setWrapStyleWord(true);
-            JScrollPane scrollPane = new JScrollPane(chatArea);
+            // chatArea = new JTextArea();
+            // chatArea.setEditable(false);
+            // chatArea.setLineWrap(true);
+            // chatArea.setWrapStyleWord(true);
+            chatPane = new JTextPane();
+            chatPane.setContentType("text/html");
+            chatPane.setEditable(false);
+            JScrollPane scrollPane = new JScrollPane( chatPane );
+            // JScrollPane scrollPane = new JScrollPane(chatArea);
 
             inputField = new JTextField();
 
@@ -88,8 +99,8 @@ public class ChatArtifact extends GUIArtifact {
         @INTERNAL_OPERATION
         private void sendMessage() {
             String message = inputField.getText();
-            if (!message.trim().isEmpty()) {
-                appendMessage("[USER] " + message);
+            if ( !message.trim().isEmpty() ) {
+                appendMessage( "interpreter", message );
                 inputField.setText("");
             }
             try {
@@ -101,8 +112,77 @@ public class ChatArtifact extends GUIArtifact {
             log("Sent " + message);
         }
 
-        public void appendMessage(String message) {
-            chatArea.append(message + "\n");
+        public void appendMessage( String sender, String msg ) {
+            String msg_with_hm = highlight_mentions( msg );
+            String msg_class = sender.equals("interpreter") ? "sent" : "received";
+
+            String currentContent = chatPane.getText();
+            int bodyStart = currentContent.indexOf("<body>") + 6;
+            int bodyEnd = currentContent.lastIndexOf("</body>");
+            String bodyContent = currentContent.substring(bodyStart, bodyEnd);
+            String headerContent = """
+                    <html>
+                    <head>
+                    <style>
+                    span {
+                      background: #ee6c4d;
+                      color: #fff;
+                      padding: 5px 10px;
+                      margin: 0 5px;
+                    }
+                    
+                    .sent {
+                        text-align: right;
+                        background: #25a18e;
+                        padding: 10px;
+                        margin: 5px;
+                    }
+
+                    .received {
+                        text-align: left;
+                        background: #00a5cf;
+                        padding: 10px;
+                        margin: 5px;
+                    }
+
+                    .sender {
+                        font-weight: bold;
+                        font-size: 10px;
+                    }
+                    
+                    .content {
+                        font-weight: normal;
+                    }
+
+                    .chat {
+                        font-family: Roboto, sans-serif;
+                        font-size: 12px;
+                        padding: 5px;
+                    }
+                    </style>
+                    </head>
+                    <body>
+                    <div class="chat">
+                    """;
+            String sender_div = "<div class='sender'> " + sender + "</div>";
+            String content_div = "<div class='content'>" + msg_with_hm + "</div>";
+            String msg_div = "<div class='" + msg_class + "'>" + sender_div + content_div + "</div>";
+            String updatedContent = bodyContent + msg_div;
+
+            chatPane.setText(headerContent + updatedContent + "</div></body></html>");
+        }
+
+        private String highlight_mentions( String msg ) {
+            Pattern pattern = Pattern.compile("@\\w+");
+            Matcher matcher = pattern.matcher( msg );
+            StringBuffer sb = new StringBuffer();
+
+            while ( matcher.find() ){
+                String mention = matcher.group();
+                matcher.appendReplacement( sb, "<span>" + mention + "</span>");
+            }
+            matcher.appendTail( sb );
+            return sb.toString();
         }
 
     }
---
+++ interpreter/src/todo.md
@@ -0,0 +1,12 @@
+# KQML Interpreter todo
+
+## Interpreter
+ - [ ] It should consider erroneous translations handling them with something like regex (?) or trying again.
+ - [ ] Literals and so on should maintain a proprietary: now they are all mixed, but they should be subdivided and considered depending on the interlocutor.
+
+## Chat
+ - [ ] It should support @at for sending messages to single agent.
+    In that case it should provide this information to agent.
+
+## Internal Actions
+ - [ ] They should remove the source from the parameter in square brackets, otherwise new beliefs are mispelled.
